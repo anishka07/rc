bc1f043436797559941e327f1f2d340b13ddf2e8
anishka07 <anishkamukherjee237@gmail.com>
2024-08-15 23:53:46 +0545

    settings

--- src/settings.py
+++ src/settings.py
@@ -19,7 +19,7 @@ category_mapping = {
     7: "Database",
     11: "Electrical Engineering",
     14: "Health and fitness",
-    19: "PMO",
+    19: "Project Management Officer",
     4: "Business Analyst",
     9: "DotNet Developer",
     2: "Automation Testing",
@@ -28,60 +28,6 @@ category_mapping = {
     5: "Civil Engineer",
     0: "Advocate",
 }
-
-sample_resume_text = """I am a data scientist specializing in machine
-learning, deep learning, and computer vision. With
-a strong background in mathematics, statistics,
-and programming, I am passionate about
-uncovering hidden patterns and insights in data.
-I have extensive experience in developing
-predictive models, implementing deep learning
-algorithms, and designing computer vision
-systems. My technical skills include proficiency in
-Python, Sklearn, TensorFlow, and PyTorch.
-What sets me apart is my ability to effectively
-communicate complex concepts to diverse
-audiences. I excel in translating technical insights
-into actionable recommendations that drive
-informed decision-making.
-If you're looking for a dedicated and versatile data
-scientist to collaborate on impactful projects, I am
-eager to contribute my expertise. Let's harness the
-power of data together to unlock new possibilities
-and shape a better future.
-Contact & Sources
-Email: 611noorsaeed@gmail.com
-Phone: 03442826192
-Github: https://github.com/611noorsaeed
-Linkdin: https://www.linkedin.com/in/noor-saeed654a23263/
-Blogs: https://medium.com/@611noorsaeed
-Youtube: Artificial Intelligence
-ABOUT ME
-WORK EXPERIENCE
-SKILLES
-NOOR SAEED
-LANGUAGES
-English
-Urdu
-Hindi
-I am a versatile data scientist with expertise in a wide
-range of projects, including machine learning,
-recommendation systems, deep learning, and computer
-vision. Throughout my career, I have successfully
-developed and deployed various machine learning models
-to solve complex problems and drive data-driven
-decision-making
-Machine Learnine
-Deep Learning
-Computer Vision
-Recommendation Systems
-Data Visualization
-Programming Languages (Python, SQL)
-Data Preprocessing and Feature Engineering
-Model Evaluation and Deployment
-Statistical Analysis
-Communication and Collaboration
-"""
 
 def clean_resume(text):
     url_pattern = re.compile(r'http\S+')
@@ -115,4 +61,3 @@ def text_transform(text: str):
 
 class Settings:
     CATEGORY = category_mapping
-    SAMPLE_TEXT = sample_resume_text
